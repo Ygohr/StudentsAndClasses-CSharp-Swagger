9b21cecf73a66beb97e01596862ebd1e35f7ec89
Ygohr Medeiros <medeirosygohr@gmail.com>
2020-10-06 22:48:43 -0300

    Add files via upload

---
+++ DesafioTecnicoMarlin/Services/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using DesafioTecnicoMarlin.Model;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+
+namespace DesafioTecnicoMarlin.Services
+{
+    public class TokenService
+    {
+        public TokenService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration Configuration { get; set; }
+        
+        public static string GenerateToken(Login login)
+        {
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, login.login.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
